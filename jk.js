d8e395f3bb78ca8569cc53f570e17359e8a0b6c2
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-11-09 13:14:04 +0100

    Update jk.js

--- jk.js
+++ jk.js
@@ -1,1 +1,11 @@
-console.log("jeff")
+const puppeteer = require('puppeteer')
+
+async function start () {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--proxy-server=20.43.156.27']
+  })
+  const page = await browser.newPage()
+  await page.goto('https://whatismyipaddress.com/')
+}
+start()
